fb24229fd2191a69b64ea91825edcb7809218beb
zakr1 <zakr1995@gmail.com>
2025-10-06 13:59:41 +0300

    add code

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class SchoolClass(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Class name")
+    student = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'role': 'STUDENT'},
+        related_name='classes'
+    )
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Subject name")
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    topic = models.CharField(max_length=100, unique=True, verbose_name="Lesson topic")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='lessons')
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE, related_name='lessons')
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'role': 'TEACHER'},
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
+    homework = models.TextField(blank=True, verbose_name="Lesson topicHomework")
+    date = models.DateField(verbose_name="Date and time of the lesson")
+
+    def __str__(self):
+        return f"{self.topic} - {self.subject.name} ({self.date.strftime('%Y-%m-%d')})"
+
+class Grade(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='grades'
+    )
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='grades')
+    grade = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)],
+        verbose_name="Grade value"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+        unique_together = ('student', 'lesson')
+
+        def __str__(self):
+            return f"{self.student.username} - {self.lesson.topic}: {self.grade}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = 'STUDENT', 'Student'
+        TEACHER = 'TEACHER', 'Teacher'
+        ADMIN = 'ADMIN', 'Admin'
+
+    role = models.CharField(max_length=50, choices=Role.choices, default=Role.STUDENT)
+    birth_date = models.DateField(null=True, blank=True)
