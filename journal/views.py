63f827710d99cdf8a52ea4180de103185c9d7f9b
zakr1 <zakr1995@gmail.com>
2025-10-10 18:16:01 +0300

    fix code

--- journal/views.py
+++ journal/views.py
@@ -4,10 +4,12 @@ from .models import Lesson, Grade, SchoolClass
 from .forms import LessonForm, GradeForm
 from users.models import User
 
+
 def get_user_model():
     teacher = User.objects.filter(role=User.Role.TEACHER).first()
     student = User.objects.filter(role=User.Role.STUDENT).first()
     return teacher, student
+
 
 def teacher_lesson_list(request):
     teacher, _ = get_user_model()
@@ -17,11 +19,21 @@ def teacher_lesson_list(request):
     lessons = Lesson.objects.filter(teacher=teacher).order_by('-date')
     return render(request, 'journal/teacher_lesson_list.html', {'lessons': lessons})
 
+
 def teacher_lesson_create(request):
+    if request.method == 'POST':
+        form = LessonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('teacher_lesson_list')
+    else:
+        form = LessonForm()
+    return render(request, 'journal/teacher_lesson_form.html', {'form': form})
 
 
 def lesson_detail(request, lesson_id):
     return HttpResponse(f"<h1>Details of lesson {lesson_id}</h1>")
+
 
 def grade_list(request):
     return HttpResponse("<h1>List of grades</h1>")
