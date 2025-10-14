8f92206de92d25d3a7b5e170eaf6571dccf4f17a
zakr1 <zakr1995@gmail.com>
2025-10-14 13:25:26 +0300

    fix code

--- journal/views.py
+++ journal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Lesson, Grade
 from .forms import LessonForm, GradeForm
 from users.models import User
